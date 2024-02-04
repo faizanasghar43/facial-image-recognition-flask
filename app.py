36ffdd7c613fdb37ae10aa1fd5b4f99fa60fa668
Muhammad Faizan Asghar <81610362+faizanasghar43@users.noreply.github.com>
2024-02-04 18:23:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,6 @@ def add_user_info():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
